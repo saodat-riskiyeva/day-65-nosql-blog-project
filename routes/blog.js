83c41b1d9a7b5e0a37b8a412876f4ad9eba529f5
SR <saodat.riskiyeva@gmail.com>
2025-09-19 23:19:42 -0400

    updating a date

--- routes/blog.js
+++ routes/blog.js
@@ -94,6 +94,7 @@ router.post("/posts/:id/edit", async function (req, res) {
     title: req.body.title,
     summary: req.body.summary,
     body: req.body.content,
+    date: new Date(),
   };
 
   const result = await db
